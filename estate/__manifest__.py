e15cfbaccbfa60aa1f85c0c7c10bd1679c9c85d8
Joshua Diaz <joshuadiaz@JoshuaD.localdomain>
2025-11-20 01:08:15 +0100

    add: update modules

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,8 +1,8 @@
 {
-    "name" : "Real Estate",
-    "summary" : "Test Module",
-    "version" : "19.0.0.0.0",
-    "license" : "OEEL-1",
-    "depends" : ["crm"],
-    "data" : []
+    'name' : 'Real Estate',
+    'summary' : 'Test Module',
+    'version' : '19.0.0.0.0',
+    'license' : 'OEEL-1',
+    'depends' : ['crm'],
+    'data' : []
 }
